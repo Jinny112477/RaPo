99f06d15efb01abfacfec3a77189ae81db0ab39b
Jinny112477 <pareenaginnie@gmail.com>
2026-04-30 07:29:53 +0700

    Update page.tsx

--- rapo/app/(dashboard)/activities/page.tsx
+++ rapo/app/(dashboard)/activities/page.tsx
@@ -1,21 +1,21 @@
-// // หน้า รายการ Activity ทั้งหมด ที่ DP เข้ามาดูได้(เฉพาะactive, มีview edit delete)
+// หน้า รายการ Activity ทั้งหมด ที่ DP เข้ามาดูได้(เฉพาะactive, มีview edit delete)
 
-// 'use client';
+'use client';
 
-// import { useRopa } from '@/lib/ropaContext';
-// import { ActivityTable } from '@/components/ActivityTable';
+import { useRopa } from '@/lib/ropaContext';
+import { ActivityTable } from '@/components/ActivityTable';
 
-// export default function ActivitiesPage() {
-//     const { activities } = useRopa();
+export default function ActivitiesPage() {
+    const { activities } = useRopa();
 
-//     console.log("ALL ACTIVITIES:", activities);
-//     console.log("STATUS LIST:", activities.map(a => a.status));
-//     console.log("FIRST ITEM:", activities[0]);
+    console.log("ALL ACTIVITIES:", activities);
+    console.log("STATUS LIST:", activities.map(a => a.status));
+    console.log("FIRST ITEM:", activities[0]);
 
-//     //เอาเฉพาะ ACTIVE เท่านั้น
-//     const activeActivities = activities.filter(
-//         (a) => a.status?.trim().toUpperCase() === 'ACTIVE'
-//     );
+    //เอาเฉพาะ ACTIVE เท่านั้น
+    const activeActivities = activities.filter(
+        (a) => a.status?.trim().toUpperCase() === 'ACTIVE'
+    );
 
-//     return <ActivityTable activities={activeActivities} />;
-// }
+    return <ActivityTable activities={activeActivities} />;
+}
